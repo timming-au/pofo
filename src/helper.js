6820a513d7f195828d06d3f4eadc964b7dd8e364
ming <timming.au@gmail.com>
2023-04-04 23:53:10 +0800

    added helper func ported from svelte version

---
+++ src/helper.js
@@ -0,0 +1,9 @@
+export const randHSL = {
+  default: function(){
+    return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`
+  },
+  pastel: function(){
+    return `hsl(${Math.floor(Math.random() * 360)}, ${Math.random() * 10 + 80}%, 75%)`
+  }
+  
+}
